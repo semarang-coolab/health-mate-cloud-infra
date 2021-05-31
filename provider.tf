e4e9202a9987a55d8d5008f56d7c772be3788cac
muktiarafi <muktiarafi@gmail.com>
2021-05-31 10:51:48 +0700

    google beta provider

--- provider.tf
+++ provider.tf
@@ -5,3 +5,11 @@ provider "google" {
   region  = "asia-southeast2"
   zone    = "asia-southeast2-a"
 }
+
+provider "google-beta" {
+  credentials = file(var.gcp_service_account)
+
+  project = var.gcp_project_id
+  region  = "asia-southeast2"
+  zone    = "asia-southeast2-a"
+}
