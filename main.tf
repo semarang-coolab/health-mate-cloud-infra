1da5249458890c6fb4956711881f9b05e6f763c9
muktiarafi <muktiarafi@gmail.com>
2021-05-31 13:58:28 +0700

    shorter vpc access connector to avoid name error

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ module "jwt_key" {
 
 resource "google_vpc_access_connector" "private" {
   provider      = google-beta
-  name          = "semarang-coolab-healthmate-dev-conn"
+  name          = "healthmate-dev-conn"
   ip_cidr_range = "10.123.0.0/28"
   network       = google_compute_network.net_one.name
   machine_type  = "f1-micro"
